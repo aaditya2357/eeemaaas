4697098e4b14895b275dd5b61b6a308ae0b269bd
Unero <ramadhanbhg@gmail.com>
2020-05-06 09:24:39 +0700

    Formating Code

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask
 
-
-# configuration
-DEBUG = True
-
-# instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
 
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
+@app.route('/easter-eggs')
+def easter():
+    return 'Wow you found me'
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-        return jsonify('pong!')
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/1337')
+def mastah():
+    return 'i am l33t'
--- getEmas.py
+++ getEmas.py
@@ -1,28 +1,38 @@
 import sqlite3
 from datetime import datetime
-from sqlite3 import Error
 from selenium.webdriver import Chrome
 
 
-def create_connection(db_file):
-    conn = sqlite3.connect(db_file)
-    return conn
-
-
-def checking(price):
+# Inserting all data to database
+def insert(price, price_p):
     tanggal = datetime.today().strftime('%d-%m-%Y')
     buy_price = price[0]
+    buy_before = price_p[0]
     sale_price = price[1]
-
-    sql = 'INSERT INTO emas VALUES ( {}, {}, {})'.format(str(tanggal), str(buy_price), str(sale_price))
-    print(sql)
+    sale_before = price_p[1]
+    buy_change = ((buy_price - buy_before)/abs(buy_before))*100
+    sale_change = ((sale_price - sale_before) / abs(sale_before)) * 100
+    sql = "INSERT INTO emas(last_update, buy_price, buy_b, buy_c, sale_price, sale_b, sale_c ) " \
+          "VALUES ( \"{}\", {}, {}, {}, {}, {}, {})"\
+        .format(str(tanggal), str(buy_price), str(buy_before), str(buy_change), str(sale_price), str(sale_before), str(sale_change))
     cur = conn.cursor()
     cur.execute(sql)
     conn.commit()
     conn.close()
-    return cur.lastrowid
 
 
+# Get Current Gold Price before current time
+# (Yesterday Price) to insert it into Current time
+def getVal():
+    price_before = "SELECT buy_price, sale_price FROM emas ORDER BY id DESC"
+    cur = conn.cursor()
+    cur.execute(price_before)
+    result = cur.fetchall()
+    conn.commit()
+    return result[0]
+
+
+# Get Current Value Of Gold
 def updater():
     browser = Chrome()
     browser.get('https://www.tokopedia.com/emas/harga-hari-ini/')
@@ -30,25 +40,18 @@ def updater():
     price = []
     for i in buy_price:
         price.append(i.text)
-    buy_price = price[0]
-    sale_price = price[1]
 
-    buy_price = buy_price[2:]
-    sale_price = sale_price[2:]
-
-    buy_price = buy_price.replace(".", "")
-    sale_price = sale_price.replace(".", "")
-
-    price.clear()
-
-    price.append(int(buy_price))
-    price.append(int(sale_price))
+    price = [x[2:] for x in price]
+    price = [x.replace(".", "") for x in price]
+    price = [int(x) for x in price]
 
     browser.close()
     return price
 
 
 if __name__ == '__main__':
-    conn = create_connection('mydatabase.db')
-    checking(updater())
-
+    conn = sqlite3.connect('mydatabase.db')
+    price = updater()
+    price_p = getVal()
+    insert(price, price_p)
+    print("=== Scrape Done ===")
